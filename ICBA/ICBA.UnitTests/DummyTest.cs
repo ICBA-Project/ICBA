1a9f0dbc118d3046d9810b9125cbca17a15e7769
Georgi Georgiev <georgiwess@gmail.com>
2017-11-08 15:11:15 +0200

    Add continuous integration with Jenkins, add a DummyTest for it to test

---
+++ ICBA/ICBA.UnitTests/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ICBA.UnitTests
+{
+    [TestFixture]
+    class DummyTest
+    {
+        [Test]
+        public void DummyTestOne()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
