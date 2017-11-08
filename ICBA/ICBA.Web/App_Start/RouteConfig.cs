98e0af803767f79240c32b4c7844c124ce65eb4a
Georgi Georgiev <georgiwess@gmail.com>
2017-11-08 19:48:04 +0200

    Set routes.LowercaseUrls to true, clear ViewEngines and add RazorViewEngine

--- ICBA/ICBA.Web/App_Start/RouteConfig.cs
+++ ICBA/ICBA.Web/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace ICBA.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.LowercaseUrls = true;
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
