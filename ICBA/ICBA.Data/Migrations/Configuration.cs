abdf14dfc7e828dbc0c62ef2ad53eb9e569bb78d
Georgi Georgiev <georgiwess@gmail.com>
2017-11-08 22:10:37 +0200

    Add Admin role and admin User seeding

--- ICBA/ICBA.Data/Migrations/Configuration.cs
+++ ICBA/ICBA.Data/Migrations/Configuration.cs
@@ -1,11 +1,14 @@
+using ICBA.Data.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
 namespace ICBA.Data.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<ICBA.Data.ApplicationDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<ICBA.Data.ApplicationDbContext>
     {
         public Configuration()
         {
@@ -14,18 +17,32 @@ namespace ICBA.Data.Migrations
 
         protected override void Seed(ICBA.Data.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if (!context.Roles.Any())
+            {
+                context.Roles.Add(new IdentityRole() { Name = "Admin" });
+                context.SaveChanges();
+            }
+            if (context.Users.Select(m => m.Roles.Count > 0).Count() == 0)
+            {
+                PasswordHasher passwordHasher = new PasswordHasher();
+                string passwordHash = passwordHasher.HashPassword("ICBA-DB");
+                ApplicationUser user = new ApplicationUser
+                {
+                    Email = "ICBA-DB@db.owner",
+                    UserName = "ICBA-DB",
+                    PasswordHash = passwordHash,
+                    SecurityStamp = Guid.NewGuid().ToString()
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                };
+                context.Users.Add(user);
+                IdentityRole role = context.Roles.Where(m => m.Name == "Admin").Single();
+                user.Roles.Add(new IdentityUserRole()
+                {
+                    RoleId = role.Id,
+                    UserId = user.Id
+                });
+                context.SaveChanges();
+            }
         }
     }
 }
--- ICBA/ICBA.Web/Global.asax.cs
+++ ICBA/ICBA.Web/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ICBA.Data;
+using ICBA.Data.Migrations;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,6 +22,8 @@ namespace ICBA.Web
 
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
     }
 }
