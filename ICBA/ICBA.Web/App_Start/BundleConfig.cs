e53a3232c06859db7f44814ef491933bcd903752
Georgi Georgiev <georgiwess@gmail.com>
2017-11-09 06:53:43 +0200

    Add a better table visualisation

--- ICBA/ICBA.Web/App_Start/BundleConfig.cs
+++ ICBA/ICBA.Web/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace ICBA.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/sidebar.css"));
+                      "~/Content/sidebar.css",
+                      "~/Content/table.css"));
         }
     }
 }
