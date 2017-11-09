2c373621db3dee0f7c37f645a9e31a9ef22f908a
Georgi Georgiev <georgiwess@gmail.com>
2017-11-09 03:26:00 +0200

    Add DB models // no validation as of right now

---
+++ ICBA/ICBA.Data/Models/Sensor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICBA.Data.Models
+{
+    public class Sensor
+    {
+        public Sensor()
+        {
+            this.SharedWithUsers = new HashSet<User>();
+            this.SensorHistory = new HashSet<SensorHistory>();
+        }
+
+        public Guid Id { get; set; }
+
+        public string SensorName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+        public string MeasureType { get; set; }
+
+        public int PollingInterval { get; set; }
+
+        public bool AccessIsPublic { get; set; }
+
+        public int MinRange { get; set; }
+
+        public int MaxRange { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        [Required]
+        public string OwnerUsername { get; set; }
+
+        public virtual ICollection<User> SharedWithUsers { get; set; }
+
+        public virtual User Owner { get; set; }
+
+        public virtual ICollection<SensorHistory> SensorHistory { get; set; }
+    }
+}
---
+++ ICBA/ICBA.Data/Models/SensorHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICBA.Data.Models
+{
+    public class SensorHistory
+    {
+        public Guid Id { get; set; }
+
+        public int SensorId { get; set; }
+
+        public string Value { get; set; }
+
+        public DateTime When { get; set; }
+
+        public int Persistence { get; set; }
+
+        public virtual Sensor Sensor { get; set; }
+    }
+}
