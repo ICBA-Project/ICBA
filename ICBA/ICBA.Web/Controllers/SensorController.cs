4d826c567a012448ab2fcf3515485514d73a6c09
Georgi Georgiev <georgiwess@gmail.com>
2017-11-11 21:41:28 +0200

    Add SensorController, add action method for the win service

---
+++ ICBA/ICBA.Web/Controllers/SensorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ICBA.Services;
+
+namespace ICBA.Web.Controllers
+{
+    public class SensorController : Controller
+    {
+        private SensorsService sensorsService;
+
+        public SensorController(SensorsService sensorsService)
+        {
+            this.sensorsService = sensorsService;
+        }
+
+        public ActionResult WinService()
+        {
+            if (this.HttpContext.Request.Headers["auth-token"] == "0b3fb7f14591-cf7b-2834-d1e5-ef64c4e8")
+            {
+                sensorsService.WinService();
+            }
+            return View();
+        }
+    }
+}
