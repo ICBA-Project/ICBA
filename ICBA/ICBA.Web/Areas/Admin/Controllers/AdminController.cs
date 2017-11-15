e0e30bb1f9a486817d3b1b777976de7e9a6bb98e
Georgi Georgiev <georgiwess@gmail.com>
2017-11-16 01:42:09 +0200

    Add last features and functionality before starting w/ unit testing

--- ICBA/ICBA.Web/Areas/Admin/Controllers/AdminController.cs
+++ ICBA/ICBA.Web/Areas/Admin/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using ICBA.Data;
 using ICBA.Data.Models;
 using ICBA.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,8 +17,9 @@ namespace ICBA.Web.Areas.Admin.Controllers
         private ApplicationDbContext dbContext;
         public AdminController(ApplicationDbContext dbContext)
         {
-            this.dbContext = dbContext;
             Guard.WhenArgument(dbContext, "dbContext").IsNull().Throw();
+
+            this.dbContext = dbContext;
         }
 
         public ActionResult AdminPanel()
@@ -29,8 +32,38 @@ namespace ICBA.Web.Areas.Admin.Controllers
 
         public ActionResult EditUser()
         {
-            return View();
+            IEnumerable<ApplicationUser> usersinDb = dbContext.Users.ToList();
+            return View("EditedUser");
         }
 
+        //[HttpPost]
+        //[Authorize]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult EditUser(ApplicationUser user)
+        //{
+        //    // I'm sorry, Vik, but it's Wed 15 Nov 21:28 right now. Don't have enough time to decouple right now! :(
+        //    ApplicationDbContext userscontext = new ApplicationDbContext();
+        //    var userStore = new UserStore<ApplicationUser>(userscontext);
+        //    var userManager = new UserManager<ApplicationUser>(userStore);
+
+        //    var dbUser = this.dbContext.Users.First(u => u.UserName == user.UserName);
+        //    if (user.Email == "on")
+        //    {
+        //        if (!userManager.IsInRole(user.Id, "Admin"))
+        //        {
+        //            userManager.AddToRole(user.Id, "Admin");
+        //        }
+        //        this.dbContext.SaveChanges();
+        //    } else if (user.Email == "off")
+        //    {
+        //        if (userManager.IsInRole(user.Id, "Admin"))
+        //        {
+        //            userManager.RemoveFromRole(user.Id, "Admin");
+        //        }
+        //        this.dbContext.SaveChanges();
+        //    }
+
+        //    return this.View("EditedUser");
+        //}
     }
 }
