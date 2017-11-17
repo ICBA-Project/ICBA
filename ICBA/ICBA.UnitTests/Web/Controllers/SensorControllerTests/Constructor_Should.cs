e5d6d3aa382bbf6ecea852e56d0d9d760367e02f
Georgi Georgiev <georgiwess@gmail.com>
2017-11-17 15:31:20 +0200

    Add constructor tests

--- ICBA/ICBA.UnitTests/Web/Controllers/SensorControllerTests/Constructor_Should.cs
+++ ICBA/ICBA.UnitTests/Web/Controllers/SensorControllerTests/Constructor_Should.cs
@@ -1,46 +1,19 @@
-﻿//using ICBA.Data;
-//using ICBA.Services;
-//using ICBA.Web.Controllers;
-//using Moq;
-//using NUnit.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net.Http;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using ICBA.Data;
+using ICBA.Services;
+using ICBA.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace ICBA.UnitTests.Controllers.SensorControllerTests
-//{
-//    [TestFixture]
-//    public class Constructor_Should
-//    {
-//        [Test]
-//        public void ReturnInstance_WhenParameterIsCorrect()
-//        {
-//            // Arrange
-//            Mock<ApplicationDbContext> mockApplicationDbContext = new Mock<ApplicationDbContext>();
-//            SlackService slackService = new SlackService();
-//            Mock<SensorsService> mockSensorService = new Mock<SensorsService>(mockApplicationDbContext.Object);
-
-//            // Act
-//            SensorController instance = new SensorController(mockSensorService.Object, mockApplicationDbContext.Object, slackService);
-
-//            // Assert
-//            Assert.IsNotNull(instance);
-//        }
-
-//        [Test]
-//        public void ThrowArgumentNullException_WhenParametersAreNotCorrect()
-//        {
-//            // Arrange
-//            Mock<ApplicationDbContext> mockApplicationDbContext = new Mock<ApplicationDbContext>();
-//            Mock<SensorsService> mockSensorApiService = new Mock<SensorsService>(mockApplicationDbContext.Object);
-//            SlackService slackService = new SlackService();
-//            // Act
-//            // Assert
-//            Assert.Throws<ArgumentNullException>(() => new SensorController(null, mockApplicationDbContext.Object,slackService));
-//            Assert.Throws<ArgumentNullException>(() => new SensorController(mockSensorApiService.Object, null, slackService));
-//        }
-//    }
-//}
+namespace ICBA.UnitTests.Controllers.SensorControllerTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+    }
+}
