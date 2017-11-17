a4b81ccfaff7223372a3cf16327ae7bdbf51510f
DESKTOP-CFBG69B\IliyaDamyanov <i.s.damyanov@gmail.com>
2017-11-17 10:26:29 +0200

    Own sensors test- oshte po jalyk opit

--- ICBA/ICBA.UnitTests/Controllers/SensorControllerTests/OwnSensors_Should.cs
+++ ICBA/ICBA.UnitTests/Controllers/SensorControllerTests/OwnSensors_Should.cs
@@ -12,6 +12,9 @@ using System.Text;
 using System.Threading.Tasks;
 using TestStack.FluentMVCTesting;
 using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+using System.Web;
+using System.Security.Principal;
 
 namespace ICBA.UnitTests.Controllers.SensorControllerTests
 {
@@ -27,24 +30,26 @@ namespace ICBA.UnitTests.Controllers.SensorControllerTests
             SlackService slackService = new SlackService();
             Mock<SensorsService> mockSensorService = new Mock<SensorsService>(mockApplicationDbContext.Object, slackService);
 
-            string userId = "userID";
+            string userId = "";
             Sensor sensor1 = new Sensor() { OwnerId = userId };
+
             // Act
 
             List<Sensor> sensorsInDbMock = new List<Sensor> { sensor1 };
-            List<ApplicationUser> usersInDbMock = new List<ApplicationUser> { user };
-
-            var sensorsInDb = new Mock<DbSet<Sensor>>().SetupData(sensorsInDbMock);
-            var usersInDb = new Mock<DbSet<ApplicationUser>>().SetupData(usersInDbMock);
-
-            mockApplicationDbContext.SetupGet(s => s.Users).Returns(usersInDb.Object);
+            var sensorsInDb = new Mock<DbSet<Sensor>>().SetupData(sensorsInDbMock);          
             mockApplicationDbContext.SetupGet(s => s.Sensors).Returns(sensorsInDb.Object);
-          
+
+            var fakeHttpContext = new Mock<HttpContextBase>();
+            var fakeIdentity = new GenericIdentity(userId);
+            var principal = new GenericPrincipal(fakeIdentity, null);
+            fakeHttpContext.Setup(t => t.User).Returns(principal);
+            Mock<ControllerContext> mockControllerContext = new Mock<ControllerContext>();
+            mockControllerContext.Setup(t => t.HttpContext).Returns(fakeHttpContext.Object);
 
             SensorController sensorController = new SensorController(mockSensorService.Object, mockApplicationDbContext.Object, slackService);
+            
+            sensorController.ControllerContext = mockControllerContext.Object;
 
-            var user = sensorController.User;
-            user.Identity. = userId;
 
             //Assert
             sensorController.WithCallTo(s => s.OwnSensors()).ShouldRenderView("SensorsDisplay")
